d12f9a4209f663ca90da06d8ba3b931928937f6c
Yash-Sharma2 <myidysharma@gmail.com>
2023-09-08 16:27:58 +0530

    Added Bubble sort Algorithm

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import './Components/Visual/visualComponent';
+import VisualComponent from './Components/Visual/visualComponent';
 
 function App() {
   return (
     <div className="App">
-      
+      <VisualComponent/>
     </div>
   );
 }
